504b60e9a9d5b3fae4d3c0c2d9b29c888da0dd74
Nam Kha Nguyen <namnamkha@gmail.com>
2018-05-21 17:30:04 +0000

    2.0

--- public/js/film.js
+++ public/js/film.js
@@ -1,3 +1,11 @@
-$(document).ready(function () {
-  $('.js-video').fadeIn(3000);
-});
+// $(document).ready(function () {
+//   $('.js-video').fadeIn(3000);
+// });
+
+// $(".lazy").recliner({
+//     attrib: "data-src", // selector for attribute containing the media src
+//     throttle: 300,      // millisecond interval at which to process events
+//     threshold: 100,     // scroll distance from element before its loaded
+//     printable: true,    // be printer friendly and show all elements on document print
+//     live: true          // auto bind lazy loading to ajax loaded elements
+// });
